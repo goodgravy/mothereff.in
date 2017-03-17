05d82835a79ed5d4457ab2336cb82201a1df6093
James Brady <james@jmsbrdy.com>
2017-03-17 10:55:41 +0100

    feat: binary encoding, not hex, in UTF8 demo

--- utf-8/eff.js
+++ utf-8/eff.js
@@ -3,27 +3,7 @@
 	var textareas = document.getElementsByTagName('textarea');
 	var decoded = textareas[0];
 	var encoded = textareas[1];
-	var permalink = document.getElementById('permalink');
-	// https://mathiasbynens.be/notes/localstorage-pattern
-	var storage = (function() {
-		var uid = new Date;
-		var storage;
-		var result;
-		try {
-			(storage = window.localStorage).setItem(uid, uid);
-			result = storage.getItem(uid) == uid;
-			storage.removeItem(uid);
-			return result && storage;
-		} catch (exception) {}
-	}());
 	var stringFromCharCode = String.fromCharCode;
-
-	function encode(string) {
-		// URL-encode some more characters to avoid issues when using permalink URLs in Markdown
-		return encodeURIComponent(string).replace(/['()_*]/g, function(character) {
-			return '%' + character.charCodeAt().toString(16);
-		});
-	}
 
 	function hexEscape(string) {
 		var length = string.length;
@@ -37,56 +17,35 @@
 		return result;
 	}
 
-	function update() {
-		var shouldDecode = this == encoded;
-		var value;
-		if (shouldDecode) {
-			try {
-				value = utf8.decode(eval('\'' + encoded.value + '\''));
-				decoded.value = value;
-				decoded.className = encoded.className = '';
-			} catch (exception) {
-				decoded.value = 'ERROR: invalid input';
-				decoded.className = encoded.className = 'invalid';
-			}
-		} else {
-			value = utf8.encode(decoded.value);
-			encoded.value = hexEscape(value);
-			decoded.className = encoded.className = '';
+	function binEscape(string) {
+		var result = '';
+		var symbols = Array.from(string);
+		for (var index = 0; index < symbols.length; index++) {
+			result += binEscapeSymbol(symbols[index]) + '\n';
 		}
-		value = decoded.value;
-		permalink.hash = encode(value);
-		storage && (storage.utf8 = value);
+		return result;
+	}
+
+	function binEscapeSymbol(symbol) {
+		var utf8Value = utf8.encode(symbol);
+		var result = '';
+		var bin;
+		for (var index = 0; index < utf8Value.length; index++) {
+			bin = utf8Value.charCodeAt(index).toString(2);
+			result += ('00000000' + bin).slice(-8) + ' ';
+		}
+		return result;
+	}
+
+	function update() {
+		encoded.value = binEscape(decoded.value);
+		decoded.className = encoded.className = '';
 	};
 
 	// https://mathiasbynens.be/notes/oninput
-	decoded.onkeyup = encoded.onkeyup = update;
-	decoded.oninput = encoded.oninput = function() {
-		decoded.onkeyup = encoded.onkeyup = null;
+	decoded.onkeyup = update;
+	decoded.oninput = function() {
+		decoded.onkeyup = null;
 		update.call(this);
 	};
-
-	if (storage) {
-		storage.utf8 && (decoded.value = storage.utf8);
-		update();
-	}
-
-	window.onhashchange = function() {
-		decoded.value = decodeURIComponent(location.hash.slice(1));
-		update();
-	};
-
-	if (location.hash) {
-		window.onhashchange();
-	}
-
 }(this, document));
-
-// Google Analytics
-window._gaq = [['_setAccount', 'UA-6065217-60'], ['_trackPageview']];
-(function(d, t) {
-	var g = d.createElement(t);
-	var s = d.getElementsByTagName(t)[0];
-	g.src = 'https://www.google-analytics.com/ga.js';
-	s.parentNode.insertBefore(g, s);
-}(document, 'script'));
